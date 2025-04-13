0b9d18f6a12d8e2409cfcaf56494c3a8a94bdb6c
burbschat <b.urbschat@tum.de>
2025-04-13 23:10:14 +0900

    implement a more interesting test pattern and add latching/blank bin to actually see something on the display
    good enough to verify that my pcb works

--- ledmat8x16pico.c
+++ ledmat8x16pico.c
@@ -9,9 +9,11 @@
 // the rp2350.
 #define PIN_CLK 2
 #define PIN_DATA 3
+#define PIN_LATCH 4
+#define PIN_BLANK 5
 // Seems like 10MHz is around the limit for decently shaped pulses with my
 // hardware. Have to do a proper rate test once everything is implemented.
-#define TLC59283_TX_FREQ 5000000
+#define TLC59283_TX_FREQ 500000
 
 #define PIN_LED 25
 const uint MOSFET_TEST = 15;
@@ -22,6 +24,13 @@ int main() {
     gpio_init(PIN_LED);
     gpio_set_dir(PIN_LED, GPIO_OUT);
 
+    gpio_init(PIN_LATCH);
+    gpio_set_dir(PIN_LATCH, GPIO_OUT);
+
+    gpio_init(PIN_BLANK);
+    gpio_set_dir(PIN_BLANK, GPIO_OUT);
+    gpio_put(PIN_BLANK, 0);  // Must be pulled low. Leaving it floating did cause problems (blank always asserted).
+
     gpio_init(MOSFET_TEST);
     gpio_set_dir(MOSFET_TEST, GPIO_OUT);
 
@@ -31,7 +40,9 @@ int main() {
     uint sm;
     uint offset;
 
-    uint32_t test_pattern = 0b10101010101010101010101010101010;
+    // uint32_t test_pattern = 0b10101010101010101010101010101010;
+       uint32_t test_pattern = 0b11001100110011001100110011001100;
+    //uint32_t test_pattern = 0b10000000000000001000000000000000;
 
     // This will find a free pio and state machine for our program and load it for us
     // We use pio_claim_free_sm_and_add_program_for_gpio_range (for_gpio_range variant)
@@ -44,12 +55,21 @@ int main() {
     while(1) {
         gpio_put(PIN_LED, 0);
         gpio_put(MOSFET_TEST, 0);
-        sleep_ms(1000);
+        sleep_ms(100);
         gpio_put(PIN_LED, 1);
         gpio_put(MOSFET_TEST, 1);
         // puts("Hello World\n");
+
+        uint32_t x = test_pattern, n = 1;
+        test_pattern = (x << n) | (x >> (32 - n));
+
         pio_sm_put_blocking(pio, sm, test_pattern);
-        sleep_ms(1000);
+        pio_sm_put_blocking(pio, sm, test_pattern);
+        sleep_ms(100);
+        // Latch the shifted in value. Later we want to do this using an interrupt.
+        gpio_put(PIN_LATCH, 1);
+        sleep_ms(10);
+        gpio_put(PIN_LATCH, 0);
     }
 
     // This will free resources and unload our program.

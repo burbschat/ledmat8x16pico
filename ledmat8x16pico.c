68b5c11847e63502b4ca2332cc8f40d4065259c8
burbschat <b.urbschat@tum.de>
2025-04-15 18:01:22 +0900

    implement dma for data transfer to pio

--- ledmat8x16pico.c
+++ ledmat8x16pico.c
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/pio.h"
+#include "hardware/dma.h"
 #include "tlc59283.pio.h"
 
 // Both pins must be on the same GPIO group (0->31 or 16->47) as "No single PIO
@@ -13,11 +14,73 @@
 #define PIN_BLANK 5
 #define PIN_ROWS_BASE 6
 #define N_ROWS 8
+
+#define N_DISPLAY_MODULES 2
 // Seems like 10MHz is around the limit for decently shaped pulses with my
-// hardware. Have to do a proper rate test once everything is implemented.
+// hardware. 
+// An important factor is for how long we wait 
 #define TLC59283_TX_FREQ 500000
 
 #define PIN_LED 25
+
+//                                     module n - 1        module n - 2      ...
+//                                     left       right    left       right
+volatile uint16_t test_pattern[2] = {0b1010101010101010, 0b1100000000000011};
+
+int dma_chan;
+
+void rotate_test_pattern() {
+    // Rotate a 32 bit test pattern
+    uint32_t x = *(uint32_t*)test_pattern, n = 2;
+    *(uint32_t*)test_pattern = (x << n) | (x >> (32 - n));
+}
+
+bool update_frame_callback(__unused struct repeating_timer *t) {
+    rotate_test_pattern();
+    return true;
+}
+
+void dma_handler() {
+    static bool first_run = true;
+
+    static uint8_t current_row = 0;
+    static uint8_t next_row = 1;
+
+    if (first_run) {
+        first_run = false;
+    } else {
+        // TODO: Replace with wait on TX FIFO empty, or even better, call the callback on TX FIFO empty (which should imply that the dma is finished)
+        busy_wait_us(1000); 
+        gpio_put(PIN_BLANK, 1); // Blank the display
+        // Strobe latch pin to latch shifted in value
+        gpio_put(PIN_LATCH, 1); 
+        // For some reason sleep_us here does not work (at least when I have a debugger connected)
+        busy_wait_us(10);
+        gpio_put(PIN_LATCH, 0);
+
+        // Turn off current row
+        gpio_put(PIN_ROWS_BASE + current_row, 1);
+        // Turn on next row
+        gpio_put(PIN_ROWS_BASE + next_row, 0);
+        gpio_put(PIN_BLANK, 0); // Un-blank the display
+
+        // Increase row counters
+        next_row++;
+        current_row++;
+
+        if (next_row >= N_ROWS) {
+            next_row = 0;
+        }
+
+        if (current_row >= N_ROWS) {
+            current_row = 0;
+        }
+    }
+    // Clear the interrupt request
+    dma_hw->ints0 = 1u << dma_chan;
+    // Dispatch DMA with the next data
+    dma_channel_set_read_addr(dma_chan, test_pattern, true);
+}
 
 int main() {
     stdio_init_all();
@@ -43,10 +106,6 @@ int main() {
     PIO pio = pio0;
     uint sm_tx = 0;
 
-    // uint32_t test_pattern = 0b10101010101010101010101010101010;
-    // uint32_t test_pattern = 0b10000000000000001000000000000000;
-    uint16_t test_pattern = 0b1100110011001100;
-
     // This will find a free pio and state machine for our program and load it for us
     // We use pio_claim_free_sm_and_add_program_for_gpio_range (for_gpio_range variant)
     // so we will get a PIO instance suitable for addressing gpios >= 32 if needed and supported by the hardware
@@ -58,49 +117,76 @@ int main() {
 
     sleep_ms(10);
 
-    uint8_t current_row = 0;
-    uint8_t next_row = current_row + 1;
+    // Setup DMA
+    dma_chan = dma_claim_unused_channel(true);
+    dma_channel_config c = dma_channel_get_default_config(dma_chan);
+    channel_config_set_transfer_data_size(&c, DMA_SIZE_16); // One module has 16 columns
+    channel_config_set_read_increment(&c, true); // Increment read address after each written block
+    channel_config_set_dreq(&c, DREQ_PIO0_TX0); // Use data request signal from PIO0 TX FIFO (must be matched to used pio!)
 
-    while(1) {
-        gpio_put(PIN_LED, 0);
-        // puts("Hello World\n");
+    dma_channel_configure(
+        dma_chan,
+        &c,
+        &pio0_hw->txf[0],  // Write address (only need to set this once)
+        NULL,              // Don't provide a read address yet
+        N_DISPLAY_MODULES, // Write the same value many times, then halt and interrupt
+        false              // Don't start yet
+    );
 
-        pio_sm_put_blocking(pio, sm_tx, test_pattern << 16);
-        pio_sm_put_blocking(pio, sm_tx, test_pattern << 16);
-        sleep_us(1);
+    // Tell the DMA to raise IRQ line 0 when the channel finishes a block
+    dma_channel_set_irq0_enabled(dma_chan, true);
 
-        // Latch the shifted in value. Later we want to do this using an interrupt.
-        gpio_put(PIN_BLANK, 1); // Blank the display
-        gpio_put(PIN_LATCH, 1);
-        sleep_us(1);
-        gpio_put(PIN_LATCH, 0);
+    // Configure the processor to run dma_handler() when DMA IRQ 0 is asserted
+    irq_set_exclusive_handler(DMA_IRQ_0, dma_handler);
+    irq_set_enabled(DMA_IRQ_0, true);
 
-        // Turn off current row
-        gpio_put(PIN_ROWS_BASE + current_row, 1);
-        // Turn on next row
-        gpio_put(PIN_ROWS_BASE + next_row, 0);
-        gpio_put(PIN_BLANK, 0); // Un-blank the display
+    // Setup timer for frame refresh callback
+    struct repeating_timer timer;
+    add_repeating_timer_ms(50, update_frame_callback, NULL, &timer);
 
-        // Increase row counters
-        next_row++;
-        current_row++;
+    // Manually call the handler once, to trigger the first transfer
+    dma_handler();
 
-        if (next_row >= N_ROWS) {
-            next_row = 0;
-        }
+    /*while(1) {*/
+    /*    gpio_put(PIN_LED, 0);*/
+    /*    // puts("Hello World\n");*/
+    /**/
+    /*    pio_sm_put_blocking(pio, sm_tx, test_pattern);*/
+    /*    sleep_us(100);*/
+    /**/
+    /*    // Latch the shifted in value. Later we want to do this using an interrupt.*/
+    /*    gpio_put(PIN_BLANK, 1); // Blank the display*/
+    /*    gpio_put(PIN_LATCH, 1);*/
+    /*    sleep_us(10);*/
+    /*    gpio_put(PIN_LATCH, 0);*/
+    /**/
+    /*    // Turn off current row*/
+    /*    gpio_put(PIN_ROWS_BASE + current_row, 1);*/
+    /*    // Turn on next row*/
+    /*    gpio_put(PIN_ROWS_BASE + next_row, 0);*/
+    /*    gpio_put(PIN_BLANK, 0); // Un-blank the display*/
+    /**/
+    /*    // Increase row counters*/
+    /*    next_row++;*/
+    /*    current_row++;*/
+    /**/
+    /*    if (next_row >= N_ROWS) {*/
+    /*        next_row = 0;*/
+    /*    }*/
+    /**/
+    /*    if (current_row >= N_ROWS) {*/
+    /*        current_row = 0;*/
+    /*    }*/
+    /**/
+    /*    gpio_put(PIN_LED, 1);*/
+    /*    // sleep_ms(1);*/
+    /*}*/
 
-        if (current_row >= N_ROWS) {
-            current_row = 0;
-        }
 
-
-        // Rotate the pattern
-        uint16_t x = test_pattern, n = 1;
-        test_pattern = (x << n) | (x >> (16 - n));
-
-        gpio_put(PIN_LED, 1);
-        sleep_ms(1);
-    }
+    // Everything else from this point is interrupt-driven. The processor has
+    // time to sit and think about its early retirement -- maybe open a bakery?
+    while (true)
+        tight_loop_contents();
 
     // This will free resources and unload our program.
     // Technically the program never reaches this point but keep the line here

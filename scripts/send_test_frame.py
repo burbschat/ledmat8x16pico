656384cf12e0d3c46ec113c9b9c2f8859443f933
burbschat <b.urbschat@tum.de>
2025-05-09 20:41:41 +0900

    Add funtionality to output bitmap font frames via UART
    Uses bitmap font read from a submodule

--- scripts/send_test_frame.py
+++ scripts/send_test_frame.py
@@ -1,13 +1,28 @@
 import serial
-import struct
 import numpy as np
 import time
+from bdfparser import Font
 
 
 # Must match parameters of the pico firmware!
 N_DISPLAY_MODULES = 7
 N_ROWS = 8
 
+def interleave_uint8(x, y):
+    # Interleave bits
+    # Shamelessly stolen from http://graphics.stanford.edu/~seander/bithacks.html#Interleave64bitOps
+    # Ignore numpy overflow warnings as they always occur as a product of the following operation
+    np.seterr(over="ignore")
+    z = np.uint16(
+        ((x * np.uint64(0x0101010101010101) & np.uint64(0x8040201008040201)) * np.uint64(0x0102040810204081) >> 49)
+        & 0x5555
+        | (
+            (y * np.uint64(0x0101010101010101) & np.uint64(0x8040201008040201)) * np.uint64(0x0102040810204081)
+            >> 48
+        )
+        & 0xAAAA
+    )
+    return z
 
 def frame_buffer_insert_hex(frame, hex: np.uint64, n_module: int, r: bool, g: bool):
     for row in range(N_ROWS):
@@ -18,20 +33,19 @@ def frame_buffer_insert_hex(frame, hex: np.uint64, n_module: int, r: bool, g: bo
         if r:
             y = np.uint8(hex >> row * 8)
 
-        # Interleave bits
-        # Shamelessly stolen from http://graphics.stanford.edu/~seander/bithacks.html#Interleave64bitOps
-        # Ignore numpy overflow warnings as they always occur as a product of the following operation
-        np.seterr(over="ignore")
-        z = np.uint16(
-            ((x * np.uint64(0x0101010101010101) & np.uint64(0x8040201008040201)) * np.uint64(0x0102040810204081) >> 49)
-            & 0x5555
-            | (
-                (y * np.uint64(0x0101010101010101) & np.uint64(0x8040201008040201)) * np.uint64(0x0102040810204081)
-                >> 48
-            )
-            & 0xAAAA
-        )
-        frame[row][n_module] = z
+        frame[row][n_module] = interleave_uint8(x, y)
+
+
+def frame_buffer_insert_single(frame, frame_single, n_module: int, r: bool, g: bool):
+    # Insert non-interlaced frame (single color) into the frame buffer
+    for row in range(N_ROWS):
+        x = np.uint8(0)
+        y = np.uint8(0)
+        if g:
+            x = frame_single[row]
+        if r:
+            y = frame_single[row]
+        frame[row][n_module] = interleave_uint8(x, y)
 
 
 def transmit_frame(ser, frame):
@@ -39,14 +53,21 @@ def transmit_frame(ser, frame):
     ser.write(bytes_data)
 
 
-def main():
+def setup_serial(port="/dev/ttyACM0"):
     # Setup serial interface (adjust port as required)
-    ser = serial.Serial("/dev/ttyACM0")
+    ser = serial.Serial(port)
     ser.baudrate = 115200
     ser.timeout = 1.0
+    return ser
 
+def get_empty_frame_buffer():
     # Prepare local frame buffer array
-    frame = np.zeros((N_ROWS, N_DISPLAY_MODULES), dtype=np.uint16)
+    return np.zeros((N_ROWS, N_DISPLAY_MODULES), dtype=np.uint16)
+
+def example_static_frame():
+    ser = setup_serial()
+
+    frame = get_empty_frame_buffer()
 
     # Some hex value encoded frame data (1 value per module)
     hex_val = np.uint64(0x3C66760606663C00)
@@ -66,5 +87,31 @@ def main():
     time.sleep(ser.timeout)
 
 
+def example_static_text():
+    # Load bitmap font
+    font = Font("./fonts/ibmfonts/bdf/ib8x8u.bdf")
+
+    # Text to display
+    text = "hello  "
+
+    ser = setup_serial()
+
+    frame = get_empty_frame_buffer()
+
+    for i in range(N_DISPLAY_MODULES):
+        char_data = font.glyph(text[i % len(text)]).draw().todata()
+        # Convert to actually numbers (from strings)
+        char_data = [np.uint8(int(char, 2)) for char in char_data]
+        frame_buffer_insert_single(frame, char_data, i, False, True)
+
+    print(np.vectorize(np.binary_repr)(frame, width=16))
+
+    # Transmit the frame
+    transmit_frame(ser, frame)
+    # Apparenty not wating for a little here causes inclomplete transmission even when I set no timeout for the serial.
+    # Write should be blocking, so not sure what is the problem here.
+    time.sleep(ser.timeout)
+
+
 if __name__ == "__main__":
-    main()
+    example_static_text()

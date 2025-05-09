092695e95dd3e517e22b8a78ebb6e514cff9a395
burbschat <b.urbschat@tum.de>
2025-05-09 11:20:26 +0900

    Fix wrong config used for dat asize and switch to 8 bit

--- ledmat8x16pico.c
+++ ledmat8x16pico.c
@@ -241,7 +241,8 @@ int main() {
     // Setup DMA for use with UART
     uartdma_chan = dma_claim_unused_channel(true);
     dma_channel_config uartdma_conf = dma_channel_get_default_config(uartdma_chan);
-    channel_config_set_transfer_data_size(&piodma_conf, DMA_SIZE_16); // One module has 16 columns
+    // One module has 16 columns but uart characters are 8 bit, so transfer in 8 bit chunks
+    channel_config_set_transfer_data_size(&uartdma_conf, DMA_SIZE_8);
     channel_config_set_read_increment(&uartdma_conf, false); // False is default?
     channel_config_set_write_increment(&uartdma_conf, true);
     // Consider use of ring buffer?
